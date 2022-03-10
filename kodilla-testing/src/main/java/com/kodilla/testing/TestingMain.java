aa0e253ebe16cb1cdce9bca8fb2012706ff93c3a
Softerec <software@10g.pl>
2022-03-10 17:14:34 +0100

    6.2 First unit test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,39 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calulator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        // next test ->
+        Calculator calc = new Calculator();
+
+        int calcResult;
+        calcResult = calc.add(16, 100);
+
+        if (calcResult == 116) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error! add method in Calc");
+        }
+
+        calcResult = calc.subtract(16, 100);
+
+        if (calcResult == -84) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error! subtract method in Calc");
+        }
+
     }
 }
