a916ab0aaf993b24bf6bbfc6d4245728005ec599
Softerec <software@10g.pl>
2022-03-18 08:57:16 +0100

    7.2 Nonmutable ForumUser

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -27,5 +28,8 @@ public class StreamMain {
         poemBeautifier.beautify("AnyText", (text) -> (text.toUpperCase()));
         poemBeautifier.beautify("AnyText", (text) -> (" * " + text + " * "));
         poemBeautifier.beautify("AnyText", (text) -> ("_" + text.toLowerCase() + "_"));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
