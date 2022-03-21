a21c4fc10c075e9deb59d5e9ec79db80459715ba
Softerec <software@10g.pl>
2022-03-21 10:37:35 +0100

    7.5 no-task: 7.5 with .count() in stream

--- kodilla-stream/src/test/java/com/kodilla/stream/book/BookTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/book/BookTestSuite.java
@@ -32,10 +32,9 @@ public class BookTestSuite {
         List<Book> books = bookDirectory.getList();
 
         //Then
-        int numberOfBooksPublicatedAfter2007 = IntStream.range(0, books.size()) // [1]
+        long numberOfBooksPublicatedAfter2007 = IntStream.range(0, books.size()) // [1]
                 .filter(n -> books.get(n).getYearOfPublication() > 2007)             // [2]
-                .map(n -> 1)                                                         // [3]
-                .sum();                                                              // [4]
+                .count();
         assertEquals(3, numberOfBooksPublicatedAfter2007);                      // [5]
     }
 }
