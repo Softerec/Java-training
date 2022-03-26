3e75357927992f1895778f7114f18d3b959e286d
Softerec <software@10g.pl>
2022-03-26 08:15:11 +0100

    8.2 task: try catch finally

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -2,20 +2,22 @@ package com.kodilla.exception.test;
 
 public class FirstChallenge {
 
-    public double divide(double a, double b) {
-        double result = 0;
-        try {
-            result = a / b;
-        } catch (ArithmeticException e) {
-            System.out.println("Return 0. Arithmetic exception occured: " + e);
-        } finally {
-            System.out.println("Divide result: " + result);
-            return result;
+    public double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException();
         }
+        return a / b;
     }
 
     public static void main(String[] args) {
         FirstChallenge firstChallenge = new FirstChallenge();
-        double result = firstChallenge.divide(3, 0);
+        double result = 0.0;
+        try {
+             result = firstChallenge.divide(3, 0);
+        } catch (ArithmeticException e) {
+            System.out.println("Return 0. Arithmetic exception occurred: " + e);
+        } finally {
+            System.out.println("Divide result: " + result);
+        }
     }
 }
