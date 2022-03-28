1948cd6163f075b252b9e2d3c738acf2ab3d0653
Softerec <software@10g.pl>
2022-03-28 10:20:39 +0200

    8.4 no task: correction to previous and added ifPresent

--- kodilla-exception/src/main/java/com/kodilla/optional/OptionalExample.java
+++ kodilla-exception/src/main/java/com/kodilla/optional/OptionalExample.java
@@ -6,13 +6,10 @@ import java.util.Optional;
 
 public class OptionalExample {
     public static void main(String[] args) {
-        User user = new User("user1", 30, 100, "Test");
+        User user = new User("user1");
 
         Optional<User> optionalUser = Optional.ofNullable(user);
 
-        String username =
-                optionalUser.orElse(new User("", 0, 0, "")).getUsername();
-
-        System.out.println(username);
+        optionalUser.ifPresent(u -> System.out.println(u.getName()));
     }
 }
