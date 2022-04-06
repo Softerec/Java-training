a0bc93bcea390fd105747bb8d6b84b1938c1843a
Softerec <software@10g.pl>
2022-04-06 12:17:13 +0200

    10 no task: with polymorphizm

--- kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Drawer.java
+++ kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Drawer.java
@@ -3,9 +3,12 @@ package com.kodilla.spring.intro.shape;
 public class Drawer {
 
     public void doDrawing() {
-        Circle circle = new Circle();
-        Triangle triangle = new Triangle();
-        circle.draw();
-        triangle.draw();
+        Figure figure;
+
+        figure = new Circle();
+        figure.draw();
+
+        figure = new Triangle();
+        figure.draw();
     }
 }
