17d02a5fd44dd38130caeb73cd1e115dcdcead3c
Softerec <software@10g.pl>
2022-04-11 16:06:09 +0200

    10.1 no task, example  Dependency  Injection  (DI)  / Inversion of Control (IoC)

--- kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Drawer.java
+++ kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Drawer.java
@@ -1,14 +1,11 @@
 package com.kodilla.spring.intro.shape;
 
 public class Drawer {
-
-    public void doDrawing() {
-        Shape shape;
-
-        shape = new Circle();
-        shape.draw();
-
-        shape = new Triangle();
-        shape.draw();
+    final Shape shape;
+    public Drawer(final Shape shape) {
+        this.shape = shape;
+    }
+    public String doDrawing() {
+        return shape.draw();
     }
 }
