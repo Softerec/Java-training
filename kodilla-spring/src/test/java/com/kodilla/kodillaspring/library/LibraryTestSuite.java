5a0473962b2a668113e77dbc0319009e1d90e437
Softerec <software@10g.pl>
2022-04-14 08:48:31 +0200

    10.2 not task: spring tests with given with autowired

---
+++ kodilla-spring/src/test/java/com/kodilla/kodillaspring/library/LibraryTestSuite.java
@@ -0,0 +1,29 @@
+package com.kodilla.kodillaspring.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LibraryTestSuite {
+
+    @Autowired
+    private Library library;
+
+    @Test
+    void testLoadFromDb() {
+        //Given
+        //When
+        library.loadFromDb();
+        //Then
+        //do nothing
+    }
+    @Test
+    void testSaveToDb() {
+        //Given
+        //When
+        library.saveToDb();
+        //Then
+        //do nothing
+    }
+}
